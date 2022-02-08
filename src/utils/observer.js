c10b53dc4b6a3bcc592a51360ad48470840833dd
Pavel <pl.hancharou@gmail.com>
2022-02-09 00:44:15 +0300

    fix: implement observer

--- src/utils/observer.js
+++ src/utils/observer.js
@@ -1,23 +1,22 @@
-export class Observer {
-  constructor(rootElement) {
-    this.rootElement = rootElement;
-
-    this.subscribers = [];
-    this.formParams = null;
-
-    this.subcribe = this.subcribe.bind(this);
-    this.getSubcriber = this.getSubcriber.bind(this);
+class Observer {
+  constructor() {
+    this.observers = [];
   }
 
-  subcribe(fn) {
-    this.subscribers.push(fn);
+  subscribe(fn) {
+    this.observers.push(fn);
   }
 
-  async getSubcriber(e) {
-    e.preventDefault();
+  unsubscribe(fn) {
+    this.observers = this.observers.filter((subscriber) => subscriber !== fn);
+  }
 
-    this.formParams = await this.subscribers[0]();
-
-    this.subscribers[1](this.rootElement, this.formParams);
+  async broadcast() {
+    for (const subscriber of this.observers) {
+      // eslint-disable-next-line no-await-in-loop
+      await subscriber();
+    }
   }
 }
+
+export const observer = new Observer();
